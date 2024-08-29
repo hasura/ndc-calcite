608cd2503511bcb847dc1e81dd9345d1dc5a437c
kenstott <128912107+kenstott@users.noreply.github.com>
2024-08-29 17:01:46 -0400

    Modified adapter testing tools and data files.

--- cli.sh
+++ cli.sh
@@ -1,6 +1,4 @@
-export LOCAL_PATH=./config-test
-export HASURA_PLUGIN_CONNECTOR_CONTEXT_PATH=/etc/connector
-rm -rf ${LOCAL_PATH}
-mkdir -p ${LOCAL_PATH}
-echo docker run --entrypoint ndc-calcite-cli -e HASURA_PLUGIN_CONNECTOR_CONTEXT_PATH -v ${LOCAL_PATH}:/etc/connector docker.io/kstott/meta_connector:latest update
-docker run --entrypoint ndc-calcite-cli -e HASURA_PLUGIN_CONNECTOR_CONTEXT_PATH -v ${LOCAL_PATH}:/etc/connector docker.io/kstott/meta_connector:latest update
+export HASURA_PLUGIN_CONNECTOR_CONTEXT_PATH=/Users/kennethstott/test3/app/connector/calcite
+#rm -rf ${HASURA_PLUGIN_CONNECTOR_CONTEXT_PATH}
+#mkdir -p ${HASURA_PLUGIN_CONNECTOR_CONTEXT_PATH}
+docker run --entrypoint ndc-calcite-cli -e "OTEL_LOG_LEVEL=trace" -e "OTEL_LOGS_EXPORTER=console" -e "OTEL_TRACES_EXPORTER=console" -e "RUST_LOG=debug" -e "LOG_LEVEL=all" -e HASURA_PLUGIN_CONNECTOR_CONTEXT_PATH -v ${HASURA_PLUGIN_CONNECTOR_CONTEXT_PATH}:/app/output -v ${HASURA_PLUGIN_CONNECTOR_CONTEXT_PATH}:/etc/connector:ro docker.io/kstott/meta_connector:latest update
