35b6184cb6a2033462f6c42d388bf62a7192279f
David A. Ventimiglia <davidaventimiglia@hasura.io>
2024-09-05 10:00:27 -0700

    Used a multi-line heredoc instead of a single-line string
    In patching up the `compose.yaml` file, the escaped newline (\n)
    doesn't actually work, at least not on Linux, anyway.

--- cli.sh
+++ cli.sh
@@ -12,7 +12,11 @@ mkdir -p $HASURA_PLUGIN_CONNECTOR_CONTEXT_PATH
 #mkdir -p ${HASURA_PLUGIN_CONNECTOR_CONTEXT_PATH}
 docker run --entrypoint ndc-calcite-cli -e "OTEL_LOG_LEVEL=trace" -e "OTEL_LOGS_EXPORTER=console" -e "OTEL_TRACES_EXPORTER=console" -e "RUST_LOG=debug" -e "LOG_LEVEL=all" -e HASURA_PLUGIN_CONNECTOR_CONTEXT_PATH -v "${HASURA_PLUGIN_CONNECTOR_CONTEXT_PATH}":/app/output -v "${HASURA_PLUGIN_CONNECTOR_CONTEXT_PATH}":/etc/connector:ro docker.io/kstott/meta_connector:latest update
 
-echo "include:\n  - path: ${filepath}/compose.yaml" > temp.yml
+# echo "include:\n  - path: ${filepath}/compose.yaml" > temp.yml
+cat <<EOF > temp.yml
+include:
+  - path: ${filepath}/compose.yaml
+EOF
 echo "$content" >> temp.yml
 mv temp.yml compose.yaml
 
