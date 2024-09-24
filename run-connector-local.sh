6b639ab8be33750330df3b31ec7f0fee29d9311e
kenstott <128912107+kenstott@users.noreply.github.com>
2024-09-24 13:53:14 -0400

    Fixed cassandra config issue.

--- run-connector-local.sh
+++ run-connector-local.sh
@@ -1,2 +1,2 @@
 cd adapters/$1
-OTEL_EXPORTER_OTLP_ENDPOINT=http://localhost:4317 OTEL_SERVICE_NAME=app_calcite LOG_LEVEL=debug OTEL_METRICS_EXPORTER=console OTEL_TRACES_EXPORTER=console OTEL_LOG_EXPORTER=consol RUST_LOG=info cargo run --package ndc-calcite --bin ndc-calcite -- serve --configuration=.
+OTEL_EXPORTER_OTLP_ENDPOINT=http://localhost:4317 OTEL_SERVICE_NAME=app_calcite LOG_LEVEL=debug OTEL_METRICS_EXPORTER=console OTEL_TRACES_EXPORTER=console OTEL_LOG_EXPORTER=console RUST_LOG=trace cargo run --package ndc-calcite --bin ndc-calcite -- serve --configuration=.
