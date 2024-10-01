499bdd1b4131ebb42cb0434df5f6c1e604f68ee2
David A. Ventimiglia <davidaventimiglia@hasura.io>
2024-10-01 06:26:59 -0700

    Disabled multi-platform Docker build for now

--- build-local.sh
+++ build-local.sh
@@ -12,7 +12,7 @@ cd ../..
 #  -H "X-GitHub-Api-Version: 2022-11-28" \
 #  https://api.github.com/repos/hasura/ndc-calcite/releases/latest)
 #TAG=$(echo "$release_info" | grep 'tag_name' | awk -F':' '{print $2}' | tr -d ' "",')
-docker build . --platform linux/arm64 -t ghcr.io/hasura/ndc-calcite:latest
+docker build . -t ghcr.io/hasura/ndc-calcite:latest
 #docker buildx build --platform linux/arm64 --output type=oci,dest=./image.tar .
 #docker tag ghcr.io/hasura/ndc-calcite:latest ghcr.io/hasura/ndc-calcite:"$TAG"
 #docker push ghcr.io/hasura/ndc-calcite:latest
