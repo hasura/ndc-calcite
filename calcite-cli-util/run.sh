054634d05bf3959584b2ab81ab01ce87801b702b
Karthikeyan C <karthikeyan@hasura.io>
2025-01-20 12:10:48 +0530

    unstage calcite-cli-util

--- calcite-cli-util/run.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-JAVA_OPTS="--add-opens=java.base/java.nio=ALL-UNNAMED --add-opens=java.base/java.lang=ALL-UNNAMED --add-opens=java.base/java.util=ALL-UNNAMED --add-opens=java.base/java.lang.reflect=ALL-UNNAMED"
-
-java $JAVA_OPTS -jar target/calcite-databricks-connector-1.0-SNAPSHOT.jar "$@"
