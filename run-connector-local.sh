13b621664d18f4ec6c0d4c439a436159ad04582d
kenstott <128912107+kenstott@users.noreply.github.com>
2025-07-08 13:27:47 -0400

    splunk adapter updates

--- run-connector-local.sh
+++ run-connector-local.sh
@@ -5,6 +5,6 @@ export OTEL_LOGS_EXPORTER=console
 export OTEL_METRICS_EXPORTER=none
 export OTEL_TRACES_EXPORTER=console
 export RUST_LOG=debug
-JAR_DEPENDENCY_FOLDER=../../calcite-rs-jni/target/dependency
-CALCITE_JAR=../../calcite-rs-jni/target/calcite-rs-jni-1.0-SNAPSHOT.jar
+export JAR_DEPENDENCY_FOLDER=../../calcite-rs-jni/jni/target/dependency
+export CALCITE_JAR=../../calcite-rs-jni/target/jni/calcite-rs-jni-1.0-SNAPSHOT.jar
 OTEL_EXPORTER_OTLP_ENDPOINT=http://localhost:4317 OTEL_SERVICE_NAME=app_calcite LOG_LEVEL=debug OTEL_METRICS_EXPORTER=console OTEL_TRACES_EXPORTER=console OTEL_LOG_EXPORTER=console RUST_LOG=info cargo run --package ndc-calcite --bin ndc-calcite -- serve --configuration=.
--- test.sh
+++ test.sh
@@ -4,7 +4,7 @@ export OTEL_LOGS_EXPORTER=console
 export OTEL_METRICS_EXPORTER=none
 export OTEL_TRACES_EXPORTER=console
 export RUST_LOG=debug
-JAR_DEPENDENCY_FOLDER=../../calcite-rs-jni/target/dependency
-CALCITE_JAR=../../calcite-rs-jni/target/calcite-rs-jni-1.0-SNAPSHOT.jar
-cd adapters/$1
+export JAR_DEPENDENCY_FOLDER=../../calcite-rs-jni/jni/target/dependency
+export CALCITE_JAR=../../calcite-rs-jni/jni/target/calcite-rs-jni-1.0-SNAPSHOT.jar
+cd adapters/"$1" || exit 1
 cargo run --package ndc-calcite --bin ndc-calcite -- test --configuration=.
